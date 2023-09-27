9e54d5a8033b98fbf3dfc212d3aeb5bc5a22dd2b
Jefferson Luiz Seno <jeffersonseno@gmail.com>
2023-09-27 20:28:47 -0300

    Adicionado dia 09 verificador de palíndromo

---
+++ Mes01/Semana02/dia09.py
@@ -0,0 +1,15 @@
+"""
+        Verificado de Palíndromo
+"""
+
+def verficador_palindromo(texto):
+    texto = texto.replace(" ", "").lower()
+
+    return texto == texto[::-1]
+
+seu_texto = str(input("Digite seu texto:\n"))
+
+if verficador_palindromo(seu_texto):
+    print(f"Seu texto é palíndromo {seu_texto}")
+else:
+    print(f"Seu texto {seu_texto} não é um palíndromo")
