41a53e1cf4c2163db8818116bc50a8ec7fe10606
Jefferson Luiz Seno <jeffersonseno@gmail.com>
2023-10-04 22:52:10 -0300

    Média de 3 Números

---
+++ Mes01/Semana03/[DIA16]media_numero.py
@@ -0,0 +1,10 @@
+"""
+        Média de 3 números
+"""
+
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+num3 = float(input("Digite o terceiro número: "))
+
+media = (num1 + num2 + num3) / 3
+print(f'A média de {num1}, {num2} e {num3} é {media}')
