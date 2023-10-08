cb23fb940b9405d71df40754c50686521a5cd4b6
Jefferson Luiz Seno <jeffersonseno@gmail.com>
2023-10-08 09:05:07 -0300

    Dia 20 Texto Maluco

---
+++ Mes_01/Semana_03/dia20_texto_maluco.py
@@ -0,0 +1,26 @@
+"""
+        Texto maluco
+"""
+import random
+
+import random
+
+def historia_maluca():
+    sujeitos = ['O elefante', 'Um macaco', 'O pirata', 'Um ninja', 'Um alienígena', 'A bruxa', 'O cientista louco', 'O velho careca']
+    verbos = ['dançou', 'comeu', 'encontrou', 'hipnotizou', 'construiu', 'teleportou', 'transformou', 'correu']
+    objetos = ['uma pizza', 'um foguete', 'um tesouro', 'um dragão', 'um portal interdimensional', 'um chapéu mágico', 'um exército de robôs', 'uma privada']
+
+    historia = []
+
+    for _ in range(5):
+        evento = f"{random.choice(sujeitos)} {random.choice(verbos)} {random.choice(objetos)}."
+        historia.append(evento)
+
+    introducao = "Numa terra distante, onde a realidade e a loucura se misturam, aconteceu o seguinte:"
+    conclusao = "E assim, a louca aventura chegou ao fim, deixando todos se perguntando o que aconteceria a seguir."
+
+    historia = [introducao] + historia + [conclusao]
+
+    return "\n".join(historia)
+
+print(historia_maluca())
--- README.md
+++ README.md
@@ -35,7 +35,7 @@
 [✅] Dia 17 - Contador de Caracteres em uma Frase&nbsp;  
 [✅] Dia 18 - Jogo de Dados Simples (Simulação de um dado de 6 lados)&nbsp;  
 [✅] Dia 19 - Calculadora de Potência&nbsp;  
-[ ] Dia 20 - Concatenador de Strings&nbsp;  
+[✅] Dia 20 - Texto Maluco&nbsp;  
 [ ] Dia 21 - Verificador de CEP por API via CEP&nbsp;
 
 ---
